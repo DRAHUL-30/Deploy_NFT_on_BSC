0b5f8caddcae5adb3a7bb9f0c1c6587599127f0e
Rahulgithub07 <rahuldharmaraja0798@gmail.com>
2022-05-02 11:32:44 +0530

    bsc network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,58 @@
+require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-ethers');
+require("dotenv").config();
+const { PUBLIC_KEY, PRIVATE_KEY, API_URL } = process.env;
+// const {mnemonic} = require('./secrets.json');
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+// task("accounts", "Prints the list of accounts", async () => {
+//   const accounts = await ethers.getSigners();
+
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
+
 /**
-* @type import('hardhat/config').HardhatUserConfig
-*/
-require('dotenv').config();
-require("@nomiclabs/hardhat-ethers");
-const { API_URL, PRIVATE_KEY } = process.env;
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
 module.exports = {
-   solidity: "0.8.1",
-   defaultNetwork: "ropsten",
-   networks: {
-      hardhat: {},
-      ropsten: {
-         url: API_URL,
-         accounts: [`0x${PRIVATE_KEY}`]
+  defaultNetwork: "mainnet",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    hardhat: {},
+    testnet: {
+      url: API_URL,
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts:[`0x${PRIVATE_KEY}`]
+    },
+    mainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
+  solidity: {
+    version: "0.8.1",
+    settings: {
+      optimizer: {
+        enabled: true
       }
-   },
-}
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 20000
+  }
+};
